58a1347ca1a146e333c399aaeab73b4b109884b5
Suprayogi Budhi P <52435299+Suprayogi8@users.noreply.github.com>
2020-06-28 19:27:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,234 @@
+ <!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body style="background-color:blue">
+
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr =$alamatErr=$nomorErr=$agamaErr="";
+$name = $email = $gender = $comment = $website = $alamat =$nomor=$agama="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+	
+	if(empty($_POST["alamat"])){
+		$alamatErr="Alamat masih kosong";
+	}
+	else{
+		 $alamat = test_input($_POST["alamat"]);
+	}
+	
+	if(empty($_POST["nomor"])){
+		$alamatErr="nomor masih kosong";
+	}
+	else{
+		 $nomor = test_input($_POST["nomor"]);
+		$alamatErr="";
+	}
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL"; 
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<center><form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<h1 style="color:white"> Selamat datang di Yogi Database</h1></center>
+<div class="form-group row">
+	<center><table border="0" width=980px;>
+	<tr>
+	<td>
+	<b><label for="nama" class ="col-sm-10 col-form-label" style="margin-right:180px;" padding-left: 10778px;> Nama </label ></b>
+	<b style="margin-left:89px;">:</b>
+	<input style="margin-left:0px;width:280px; height:40px;" type="text" name="name" value="<?php echo $name;?>" class="form-control" placeholder="Masukkan Nama Anda">
+	<span class="error">* <?php echo $nameErr;?></span>
+	</td>
+	</tr>
+	</table></center>
+</div>
+<br>
+<div class="form-group row">
+	<center><table border="0" width=980px;>
+	<tr>
+	<td>
+	<b><label for="alamat" class ="col-sm-10 col-form-label" style="margin-right:280px;" padding-left: 10778px;> Alamat </label>
+	<b style="margin-left:-20px">:</b>
+	<input style="margin-right:20px;width:300px; height:40px;" type="text" name="alamat" value="<?php echo $alamat;?>" class="form-control" placeholder="Masukkan Alamat Anda">
+	<span class="error">* <?php if(empty($_POST["alamat"])){
+		$alamatErr="Alamat masih kosong";
+	}
+	else{
+		 $alamat = test_input($_POST["alamat"]);
+		 $alamatErr="";
+	}echo $alamatErr;?></span>
+	</tr>
+	</td>
+	</table></center>
+</div>
+<br>
+<div class="form-group row">
+	<center><table border="0" width=980px;>
+	<tr>
+	<td>
+	<label for="telpon" class ="col-sm-10 col-form-label" style="margin-right:260px;" padding-left: 10778px;> Telpon </label>
+	<b>:</b>
+	<input style="width:200px; height:40px;" type="number" name=nomor value="<?php echo $nomor;?>" class="form-control" placeholder="Masukkan No Telp Anda">
+	<span class="error">* <?php 
+	if(empty($_POST["nomor"])){
+		$nomorErr="nomor masih kosong";
+	}
+	else{
+		 $nomor = test_input($_POST["nomor"]);
+		 $nomorErr="";
+	}
+	echo $nomorErr;?></span>
+	</tr>
+	</td>
+	</table></center>
+</div>
+<br>
+<center><table border="0" width=980px;>
+	<tr>
+	<td>
+<div class="input">
+	Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  </tr>
+	</td>
+	</table></center>
+</div>
+<br>
+<div class="dropdown">
+	<center><table border="0" width=980px;>
+	<tr>
+	<td>
+	<label for="agama" name="Agama" class ="col-sm-10 col-form-label" style="margin-right:26	0px;" padding-left: 10778px;> Agama </label>
+	<b>:</b>
+	<select name="Agama">
+	<option value ="Islam">Islam</option>
+	<option value ="Hindu">Hindu</option>
+	<option value ="Budha">Budha</option>
+	<option value ="Kristen">Kristen</option>
+	</select>
+	<span class="error">* <?php 
+	if(empty($_POST["agama"])){
+		$agamaErr="pilihan masih kosong";
+	}
+	else{
+		 $agama = test_input($_POST["agama"]);
+		 $agamaErr="";
+	}
+	echo $agamaErr;?></span>
+	  </tr>
+	</td>
+	</table></center>
+</div>
+<br>
+<div class="form-group row">
+	<center><table border="0" width=980px;>
+	<tr>
+	<td>
+	<label for="prestasi" class ="col-sm-10 col-form-label" style="margin-right:120px;" padding-left: 10778px;> Kelebihan dan Keurangan </label>
+	<b>:</b>
+	<input style="width:400px; height:150px;" type="text" id="alamat" class="form-control" placeholder="Masukkan Kelebihan dan Kekurangan yang anda miliki">
+	 </tr>
+	</td>
+	</table></center>
+</div>
+	<br><br>
+	<center><table border="0" width=980px;>
+	<tr>
+	<td>
+  <font style="margin-right:10px;">E-mail:</font> <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  </tr>
+	</td>
+	</table></center>
+  <br><br>
+  <center><table border="0" width=980px;>
+	<tr>
+	<td>
+  <font style="margin-right:10px;">Website:</font> <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+   </tr>
+	</td>
+	</table></center>
+  <br><br>
+  <center><table border="0" width=980px;>
+	<tr>
+	<td>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <center><table border="0" width=980px;>
+	<tr>
+	<td>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+  </center>
+</form>
+
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
